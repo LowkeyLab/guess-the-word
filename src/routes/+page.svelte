dbf1f401c81008856f9782b175163760a872700f
tacascer <trandangtrithanh2000@gmail.com>
2025-01-19 12:32:01 -0500

    feat: initial markdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
-<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+<h1 class="scroll-m-20 text-4xl tracking-tight lg:text-5xl">
     Welcome to Guess The Word!
 </h1>
 
-<h2 class="scroll-m-20 text-2xl font-bold tracking-tight lg:text-3xl">
+<h2 class="scroll-m-20 text-2xl tracking-tight lg:text-3xl">
     You and another player must take turn to come up with the same word.
 </h2>
