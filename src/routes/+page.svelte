3554f1d37ca5bc4f8ef2b39ab2d45f1b8710cac6
Tim Tran <49887921+tacascer@users.noreply.github.com>
2025-01-23 06:39:10 +0000

    feat: make text on button and input form larger

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,14 +18,14 @@
 			<Form.Field {form} name="username">
 				<Form.Control>
 					{#snippet children({ props })}
-						<Form.Label>Name</Form.Label>
+						<Form.Label class="text-xl">Name</Form.Label>
 						<Input {...props} bind:value={$formData.username} />
 					{/snippet}
 				</Form.Control>
 				<Form.Description>This is your public display name.</Form.Description>
 				<Form.FieldErrors />
 			</Form.Field>
-			<Form.Button>Submit</Form.Button>
+			<Form.Button class="text-lg">Submit</Form.Button>
 		</div>
 	</form>
 </div>
