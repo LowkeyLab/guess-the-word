f03ea75132c3078faf8d0a5517995fa7f612d0bc
Tim Tran <trandangtrithanh2000@gmail.com>
2025-01-26 02:11:19 -0500

    feat: have a player waiting animation

--- src/routes/private/game/[id]/+page.svelte
+++ src/routes/private/game/[id]/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Welcome to Game</h1>
+<div class="box-border flex justify-center">
+	<div class="loader text-lg"></div>
+</div>
+
+<style>
+	.loader {
+		width: fit-content;
+		font-family: monospace;
+		clip-path: inset(0 3ch 0 0);
+		animation: l4 1s steps(4) infinite;
+	}
+	.loader:before {
+		content: 'Waiting for player...';
+	}
+	@keyframes l4 {
+		to {
+			clip-path: inset(0 -1ch 0 0);
+		}
+	}
+</style>
