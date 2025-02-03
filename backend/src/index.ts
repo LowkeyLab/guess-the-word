6b42b7fd401531e4293a3a5167dfcbd649014061
Tim Tran <trandangtrithanh2000@gmail.com>
2025-02-03 18:53:45 -0500

    feat: listen to port env var

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -68,6 +68,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
