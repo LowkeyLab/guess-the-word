01bbd35c5cd577c7c91e4d55dd7e65ddb34c166a
Tim Tran <trandangtrithanh2000@gmail.com>
2025-02-05 20:13:35 -0500

    feat: csr for individual game page

--- frontend/src/routes/private/game/[id]/+page.ts
+++ frontend/src/routes/private/game/[id]/+page.ts
@@ -3,6 +3,8 @@ import type { ServerToClientEvents, ClientToServerEvents } from '@common';
 import { Socket, io } from 'socket.io-client';
 import type { PageLoad } from './$types';
 
+export const ssr = false;
+
 export const load: PageLoad = async ({ params, data }) => {
 	const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(PUBLIC_BACKEND_URL);
 	socket.emit('joinGame', params.id, data.user.id, data.user.user_metadata.name);
